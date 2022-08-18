59c320731b397bf167d99305cfe2341c898b9702
shreyashdj <shreyashjadhav1799@gmail.com>
2022-08-19 02:46:42 +0530

    [Shreyash]:Added program to print welcome massage

---
+++ src/com/bridgelabz/addressBook/AddressBook.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.addressBook;
+
+/*
+ * @author SHREYASH JADHAV
+ */
+public class AddressBook {
+
+	public static void main(String[] args) {
+
+		System.out.println(" Welcome to Address Book Prgram");
+		
+	}
+
+}
