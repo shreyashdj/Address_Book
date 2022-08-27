e2a60fb5babf228f3577f7b3458a9820d0e4bd60
shreyashdj <shreyashjadhav1799@gmail.com>
2022-08-27 18:58:37 +0530

    [Shreyash]:Added code to print welcome message

---
+++ src/com/bridgelabz/addressBook/AddressBookMain.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.addressBook;
+
+/*
+ * @author SHREYASH JADHAV
+ */
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+
+		System.out.println(" Welcome to Address Book Program");
+
+	}
+}
