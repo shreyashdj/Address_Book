29cd43ee2475bdf991e6fb7f925ee44992c3aeb5
shreyashdj <shreyashjadhav1799@gmail.com>
2022-08-28 06:18:36 +0530

    [Shreyash]:Added code to make console for performing activities Add contact, Display contact, Exit

--- src/com/bridgelabz/addressBook/AddressBookMain.java
+++ src/com/bridgelabz/addressBook/AddressBookMain.java
@@ -7,7 +7,36 @@ public class AddressBookMain {
 
 	public static void main(String[] args) {
 
+		AddressBook addressBook = new AddressBook();			// 	created object of AddressBook class
+		
 		System.out.println(" Welcome to Address Book Program");
 
+		int option;		// variable to store value of console options
+		
+		/*
+		 *		used do-while loop and switch-case to make console for activities like
+		 *		Add contact, Display contact, Exit
+		 */
+		
+		do {
+			System.out.println("\n *** Available Options *** ");
+			System.out.println(" 1. Add Contact \n 2. Display Contact \n 3. Exit");
+			System.out.print(" Enter option to perform activity : ");
+			option = addressBook.scan.nextInt();
+			
+			switch (option) {
+			
+			case 1 -> addressBook.addContact();
+
+			case 2 -> addressBook.displayContact();
+
+			case 3 -> System.out.println("\n Exited Successfully");
+			
+			default -> System.out.print(" Option not Available \n");
+			
+			}
+			
+		} while(option != 3);
+		
 	}
 }
